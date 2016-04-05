d629c19077205de498ccaf5fae643f458ba5cf01
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-04-05 00:01:42 -0700

    Added basic login/logout pages

--- perdiem/perdiem/urls.py
+++ perdiem/perdiem/urls.py
@@ -7,6 +7,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 from django.views.generic import TemplateView
 from django.views.static import serve
 
@@ -16,7 +17,10 @@ from artist.views import CoordinatesFromAddressView, ArtistListView, \
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/?$', login, name='login'),
+    url(r'^accounts/logout/?$', logout, {'next_page': '/',}, name='logout'),
     url(r'^api/coordinates/?$', CoordinatesFromAddressView.as_view(), name='coordinates'),
+
     url(r'^artists/?$', ArtistListView.as_view(), name='artists'),
     url(r'^artist/(?P<slug>[\w_-]+)/?$', ArtistDetailView.as_view(), name='artist'),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
