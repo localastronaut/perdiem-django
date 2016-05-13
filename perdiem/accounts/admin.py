371663140013134d61360bf0a296a2bc2187ade1
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-05-12 23:29:03 -0700

    Hide python-social-auth models from admin

---
+++ perdiem/accounts/admin.py
@@ -0,0 +1,20 @@
+"""
+:Created: 12 May 2016
+:Author: Lucas Connors
+
+"""
+
+from django.contrib import admin
+from django.contrib.sites.models import Site
+
+from social.apps.django_app.default.models import (
+    Association, Nonce, UserSocialAuth
+)
+
+
+# Unregister Site and Python Social Auth models from admin
+admin.site.unregister(Site)
+for python_social_auth_model in [
+    Association, Nonce, UserSocialAuth,
+]:
+    admin.site.unregister(python_social_auth_model)
