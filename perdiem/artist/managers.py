8c86805fc845eaf175fd9ce03d13190f0174449d
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-05-14 22:12:48 -0700

    Improve performance of artist filtering by location by first drawing bounding coordinates and filtering out artists that are not within those boundaries

--- perdiem/artist/managers.py
+++ perdiem/artist/managers.py
@@ -6,14 +6,33 @@
 
 from django.db import models
 
+import geopy
 from geopy.distance import distance as calc_distance
 
 
 class ArtistQuerySet(models.QuerySet):
 
+    @staticmethod
+    def bounding_coordinates(distance, lat, lon):
+        origin = geopy.Point((lat, lon,))
+        geopy_distance = calc_distance(miles=distance)
+        min_lat = geopy_distance.destination(origin, 180).latitude
+        max_lat = geopy_distance.destination(origin, 0).latitude
+        min_lon = geopy_distance.destination(origin, 270).longitude
+        max_lon = geopy_distance.destination(origin, 90).longitude
+        return min_lat, max_lat, min_lon, max_lon
+
     def filter_by_location(self, distance, lat, lon):
+        min_lat, max_lat, min_lon, max_lon = self.bounding_coordinates(distance, lat, lon)
+        artists_within_bounds = self.filter(
+            lat__gte=min_lat,
+            lat__lte=max_lat,
+            lon__gte=min_lon,
+            lon__lte=max_lon
+        )
+
         nearby_artist_ids = []
-        for artist in self.all():
+        for artist in artists_within_bounds:
             if calc_distance((lat, lon,), (artist.lat, artist.lon,)).miles <= distance:
                 nearby_artist_ids.append(artist.id)
         return self.filter(id__in=nearby_artist_ids)
